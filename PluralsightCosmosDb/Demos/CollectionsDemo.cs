455507f428c0fa34c1d8ec7f043a30076e4673fe
Jelle Be-Value <be-value@live.nl>
2018-11-15 14:56:08 +0100

    Demo code for using Azure CosmosDb (used User-Secrets to secure access to the actual database in development env)

---
+++ PluralsightCosmosDb/Demos/CollectionsDemo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace PluralsightCosmosDb.Demos
+{
+    public static class CollectionsDemo
+    {
+        public static Uri MyDbDatabaseUri =>
+            UriFactory.CreateDatabaseUri("mydb");
+
+        public static async Task Run(DocumentClient client)
+        {
+            Debugger.Break();
+            ViewCollections(client);
+
+            await CreateCollection(client, "MyCollection1");
+            await CreateCollection(client, "MyCollection2", 25000);
+            ViewCollections(client);
+
+            await DeleteCollection(client, "MyCollection1");
+            await DeleteCollection(client, "MyCollection2");
+        }
+
+        private static void ViewCollections(DocumentClient client)
+        {
+            Console.WriteLine();
+            Console.WriteLine(">>> View Collections in mydb <<<");
+
+            var collections = client
+                .CreateDocumentCollectionQuery(MyDbDatabaseUri)
+                .ToList();
+
+            var i = 0;
+            foreach (var collection in collections)
+            {
+                i++;
+                Console.WriteLine();
+                Console.WriteLine($" Collection #{i}");
+                ViewCollection(collection);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Total collections in mydb database: {collections.Count}");
+        }
+
+        private static void ViewCollection(DocumentCollection collection)
+        {
+            Console.WriteLine($"    Collection ID: {collection.Id}");
+            Console.WriteLine($"      Resource ID: {collection.ResourceId}");
+            Console.WriteLine($"        Self Link: {collection.SelfLink}");
+            Console.WriteLine($"            E-Tag: {collection.ETag}");
+            Console.WriteLine($"        Timestamp: {collection.Timestamp}");
+        }
+
+        private static async Task CreateCollection(
+            DocumentClient client,
+            string collectionId,
+            int reservedRUs = 1000,
+            string partitionKey = "/partitionKey")
+        {
+            Console.WriteLine();
+            Console.WriteLine($">>> Create Collection {collectionId} in mydb <<<");
+            Console.WriteLine();
+            Console.WriteLine($" Throughput: {reservedRUs} RU/sec");
+            Console.WriteLine($" Partition key: {partitionKey}");
+            Console.WriteLine();
+
+            var partitionKeyDefinition = new PartitionKeyDefinition();
+            partitionKeyDefinition.Paths.Add(partitionKey);
+
+            var collectionDefinition = new DocumentCollection
+            {
+                Id = collectionId,
+                PartitionKey = partitionKeyDefinition
+            };
+            var options = new RequestOptions { OfferThroughput = reservedRUs };
+
+            var result = await client.CreateDocumentCollectionAsync(MyDbDatabaseUri, collectionDefinition, options);
+            var collection = result.Resource;
+
+            Console.WriteLine("Created new collection");
+            ViewCollection(collection);
+        }
+
+        private static async Task DeleteCollection(DocumentClient client, string collectionId)
+        {
+            Console.WriteLine();
+            Console.WriteLine($">>> Delete Collection {collectionId} in mydb <<<");
+
+            var collectionUri = UriFactory.CreateDocumentCollectionUri("mydb", collectionId);
+            await client.DeleteDocumentCollectionAsync(collectionUri);
+
+            Console.WriteLine($"Deleted collection {collectionId} from database mydb");
+        }
+
+    }
+}
--- PluralsightCosmosDb/Program.cs
+++ PluralsightCosmosDb/Program.cs
@@ -1,12 +1,120 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using PluralsightCosmosDb.Demos;
 
 namespace PluralsightCosmosDb
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static IConfigurationRoot Configuration { get; set; }
+        private static IDictionary<string, Func<DocumentClient, Task>> _demoMethods;
+
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var devEnvironmentVariable = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+            var isDevelopment = string.IsNullOrEmpty(devEnvironmentVariable) || devEnvironmentVariable.ToLower() == "development";
+
+            //Determines the working environment as IHostingEnvironment is unavailable in a console app
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            if (isDevelopment) //only add secrets in development
+            {
+                builder.AddUserSecrets(typeof(Program).Assembly);
+            }
+
+            Configuration = builder.Build();
+
+            DoWork();
         }
+
+        private static void DoWork()
+        {
+            _demoMethods = new Dictionary<string, Func<DocumentClient, Task>>
+            {
+                {"DB", DatabasesDemo.Run},
+                {"CO", CollectionsDemo.Run},
+                {"DO", DocumentsDemo.Run},
+                {"IX", IndexingDemo.Run},
+                {"UP", UsersAndPermissionsDemo.Run},
+                {"C", Cleanup.Run}
+            };
+
+            Task.Run(async () =>
+            {
+                ShowMenu();
+                while (true)
+                {
+                    Console.Write("Selection: ");
+                    var input = Console.ReadLine();
+                    // ReSharper disable once PossibleNullReferenceException
+                    var demoId = input.ToUpper().Trim();
+                    if (_demoMethods.Keys.Contains(demoId))
+                    {
+                        var demoMethod = _demoMethods[demoId];
+                        await RunDemo(demoMethod);
+                    }
+                    else if (demoId == "Q")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"?{input}");
+                    }
+                }
+            }).Wait();
+        }
+
+        private static void ShowMenu()
+        {
+            Console.WriteLine(@"Cosmos DB SQL API .NET SDK demos
+
+DB Databases
+CO Collections
+DO Documents
+IX Indexing
+UP Users & Permissions
+
+C  Cleanup
+
+Q  Quit
+");
+        }
+
+        private static async Task RunDemo(Func<DocumentClient, Task> demoMethod)
+        {
+            try
+            {
+                var endpoint = Configuration["cosmosDb:account:endpoint"];
+                var masterkey = Configuration["cosmosDb:account:masterKey"];
+                using (var client = new DocumentClient(new Uri(endpoint), masterkey))
+                {
+                    await demoMethod(client);
+                }
+            }
+            catch (Exception ex)
+            {
+                var message = ex.Message;
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                    message += Environment.NewLine + ex.Message;
+                }
+                Console.WriteLine($"Error: {message}");
+            }
+            Console.WriteLine();
+            Console.Write("Done. Press any key to continue...");
+            Console.ReadKey(true);
+            Console.Clear();
+            ShowMenu();
+        }
+
     }
 }
